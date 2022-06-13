ca860165d6cb0d1b26315b4766e71521207eafa8
gojutin <gojutin@gmail.com>
2022-06-13 14:23:55 -0400

    feat: work on about page slideshow

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,77 +1,94 @@
 import { Layout } from "components/Layout";
+import Image from "next/image";
+import Head from "next/head";
 import { SEO } from "components/SEO";
 import { SectionTitle } from "components/SectionTitle";
-import { Card } from "components/Card";
 import YouTube from "react-youtube";
-import "react-image-gallery/styles/css/image-gallery.css";
-import ImageGallery from "react-image-gallery";
+import { NextLink } from "components/NextLink";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
+const NUMBER_OF_PHOTOS = 4;
+
+const photos = Array.from(
+  { length: NUMBER_OF_PHOTOS },
+  (_, i) => `/album/${i + 1}.jpg`
+);
 
 const YoutubeVideoWithCaption = (props) => {
   const { caption, videoId } = props;
   return (
-    <figure className="max-w-4xl">
-      <YouTube videoId={videoId} loading="lazy" />
-      <figcaption className="w-full md:w-[640px] text-sm surface p-2">
-        {caption}
-      </figcaption>
+    <figure className="w-screen md:w-[640px]">
+      <iframe
+        width="640"
+        height="480"
+        src={`https://www.youtube.com/embed/${videoId}`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        loading="lazy"
+        className="w-full md:w-[640px]"
+      />
+
+      <figcaption className="text-sm surface p-2">{caption}</figcaption>
     </figure>
   );
 };
-
-const images = [
-  {
-    original:
-      "https://res.cloudinary.com/gojutin/image/upload/w_640,h_480,c_fill/v1654651607/Community%20Solidarity/Image%20Gallery/1.png",
-    thumbnail:
-      "https://res.cloudinary.com/gojutin/image/upload/w_640,h_480,c_fill/v1654651607/Community%20Solidarity/Image%20Gallery/1.png",
-  },
-  {
-    original:
-      "https://res.cloudinary.com/gojutin/image/upload/w_640,h_480,c_fill/v1654651607/Community%20Solidarity/Image%20Gallery/Poster.jpg",
-    thumbnail:
-      "https://res.cloudinary.com/gojutin/image/upload/w_640,h_480,c_fill/v1654651607/Community%20Solidarity/Image%20Gallery/Poster.jpg",
-  },
-  {
-    original:
-      "https://res.cloudinary.com/gojutin/image/upload/w_640,h_480,c_fill/v1654651607/Community%20Solidarity/Image%20Gallery/IMG_3490_1.jpg",
-    thumbnail:
-      "https://res.cloudinary.com/gojutin/image/upload/w_640,h_480,c_fill/v1654651607/Community%20Solidarity/Image%20Gallery/IMG_3490_1.jpg",
-  },
-];
 
 export default function Page() {
   return (
     <Layout>
       <SEO />
-      <Card className="texture">
-        <SectionTitle>About Community Solidarity</SectionTitle>
+      <div className="flex flex-col lg:flex-row items-start gap-8">
+        <section>
+          <SectionTitle>About Community Solidarity</SectionTitle>
 
-        <ul className="max-w-2xl list-disc ml-4 space-y-2">
-          <li>
-            <p>
-              Community Solidarity is a 501(c)3 nonprofit organization founded
-              in 2011.
-            </p>
-          </li>
-          <li>
-            <p>
-              Community Solidarity proudly holds the title of the largest
-              vegetarian hunger relief organization.
-            </p>
-          </li>
-          <li>
-            <p>
-              We are a group of volunteers who rescue and distribute fresh,
-              vegetarian groceries to families in need all across Long Island
-              and Brooklyn. The food is distributed at our five Food Shares.
-            </p>
-          </li>
-        </ul>
+          <ul className="list-disc space-y-2 pl-8">
+            <li>
+              <p>
+                Community Solidarity is a 501(c)3 nonprofit organization founded
+                in 2011.
+              </p>
+            </li>
+            <li>
+              <p>
+                Community Solidarity proudly holds the title of the largest
+                vegetarian hunger relief organization.
+              </p>
+            </li>
+            <li>
+              <p>
+                We are a group of volunteers who rescue and distribute fresh,
+                vegetarian groceries to families in need all across Long Island
+                and Brooklyn. The food is distributed at our five{" "}
+                <NextLink href="/food-shares">Food Shares</NextLink>.
+              </p>
+            </li>
+          </ul>
+        </section>
 
-        <section className="my-8">
+        <section>
+          <SectionTitle>Photos</SectionTitle>
+          <Carousel className="w-full md:w-[640px]" showThumbs={true}>
+            {photos.map((photo) => (
+              // eslint-disable-next-line @next/next/no-img-element
+              <img
+                className="w-full md:w-[640px]"
+                height="480"
+                width="640"
+                key={photo}
+                src={photo}
+                alt="CS Photos"
+              />
+            ))}
+          </Carousel>
           <SectionTitle>Videos</SectionTitle>
 
           <div className="space-y-4">
+            <YoutubeVideoWithCaption
+              caption="News 12: Making a Difference with Jon Stepanian"
+              videoId="SbZTR5rMvn4"
+            />
             <YoutubeVideoWithCaption
               caption="An inspiring 3 minute video created by volunteer, Phillip Gladkov,
             and narrated by Jon Stepanian."
@@ -93,14 +110,7 @@ export default function Page() {
             />
           </div>
         </section>
-
-        <section>
-          <SectionTitle>Gallery</SectionTitle>
-          <div className="w-[640px]">
-            <ImageGallery items={images} />
-          </div>
-        </section>
-      </Card>
+      </div>
     </Layout>
   );
 }
