f5f0f7dce62e30bd1dbdccdf7597a801f82d3e59
gojutin <gojutin@gmail.com>
2022-06-16 14:49:13 -0400

    feat: built contact page

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -9,7 +9,7 @@ type ContactMethodProps = {
 const ContactMethod = (props: ContactMethodProps) => {
   const { icon: Icon, label, children } = props;
   return (
-    <div className="flex items-center gap-4 text-xl text-sky-800">
+    <div className="flex items-center gap-4 text-xl ">
       <div className="h-8 w-8 rounded-full bg-sky-800 grid place-items-center">
         <Icon className="text-white" aria-label={label} />
       </div>
@@ -20,18 +20,18 @@ const ContactMethod = (props: ContactMethodProps) => {
 
 export const ContactInfo = () => {
   return (
-    <div className="inline-block space-y-4 rounded-xl surface-alt p-4 shadow">
+    <div className="flex flex-col md:flex-row items-center justify-around flex-wrap p-2 surface-alt texture rounded-xl">
       <ContactMethod icon={MdLocalPhone} label="Phone Number">
-        <p className="text-sky-800">631-403-0362</p>
+        <p>631-223-4370</p>
       </ContactMethod>
-      <div className="h-[1px] w-3/4 bg-gray-100" />
+
       <ContactMethod icon={MdMail} label="Email Address">
-        <p className="text-sky-800">info@communitysolidarity.com</p>
+        <p>info@communitysolidarity.com</p>
       </ContactMethod>
-      <div className="h-[1px] w-3/4 bg-gray-100" />
+
       <ContactMethod icon={HiOfficeBuilding} label="Address">
-        <p className="text-sky-800">PO Box 208</p>
-        <p className="text-sky-800">Huntington, NY 11743</p>
+        <p>PO Box 208</p>
+        <p>Huntington, NY 11743</p>
       </ContactMethod>
     </div>
   );
