1bfacac15707fa58aa6d76e4f8f079e50dfcb7e0
gojutin <gojutin@gmail.com>
2022-06-17 11:34:42 -0400

    refactor: move from google font to fontsource

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "styles/globals.css";
+import "@fontsource/bebas-neue";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,18 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head />
       <body className="surface text">
         <Main />
         <NextScript />
