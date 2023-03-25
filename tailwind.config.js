fffbab595110d3fe4cc4626caea450b9bd64d53b
just214 <21136155+just214@users.noreply.github.com>
2023-03-25 14:18:57 -0400

    style: update to Tailwind config for new font variable, refinements to base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,17 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        'display': ['Bebas Neue', "sans-serif"],
+        display: ["var(--font-bebas)", ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        'cs-pink': "#ea3968",
-        'cs-gray': "#0A0A0A",
-      }
+        "cs-pink": "#ea3968",
+        "cs-gray": "#0A0A0A",
+      },
     },
   },
-  plugins: [],
-}
+};
