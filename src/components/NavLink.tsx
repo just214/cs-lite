7a28bc3dde70773ce7356648a0bf4efbd54caebe
just214 <21136155+just214@users.noreply.github.com>
2023-03-25 18:18:09 -0400

    refactor: nav ling style refinements

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const NavLink = (props: Props) => {
   const baseClassNames =
-    "font-medium mx-2 text-2xl sm:text-lg xl:text-2xl transition-colors duration-200 ease-in-out px-6 py-2 rounded-md";
+    "font-medium mx-1 text-2xl sm:text-xl xl:text-2xl transition-colors duration-200 ease-in-out px-4 xl:px-6 py-2 rounded-md";
 
   const selectedClassNames = props.isActive
     ? " bg-gray-500/10 text-cs-pink"
