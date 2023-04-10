1e890801f8dfb52fb50321378395c409732990cf
just214 <21136155+just214@users.noreply.github.com>
2023-04-10 15:06:58 -0400

    refactor: improved Cloudinary fetch logic

--- src/scripts/fetch-cloudinary-resources.js
+++ src/scripts/fetch-cloudinary-resources.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
-require("dotenv").config();
 const cloudinary = require("cloudinary");
+require("dotenv").config();
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -8,59 +9,75 @@ cloudinary.config({
   secure: true,
 });
 
-async function fetchCloudinaryAssets(prefix, save_path) {
-  const options = {
-    type: "upload",
-    prefix,
-    max_results: 200,
-  };
+async function fetchCloudinaryAssets(config) {
   const result = [];
-  await listResources(options, save_path, result);
+  await listResources(config, result);
 }
 
-async function listResources(options, save_path, result, next_cursor) {
+async function listResources(config, result, next_cursor) {
   if (next_cursor) {
-    options["next_cursor"] = next_cursor;
+    config.options["next_cursor"] = next_cursor;
   }
-  await cloudinary.v2.api.resources(options, async function (error, res) {
-    const more = res?.next_cursor;
-    const resources = res?.resources;
+  await cloudinary.v2.api.resources(
+    config.options,
+    async function (error, res) {
+      const more = res?.next_cursor;
+      const resources = res?.resources;
 
-    for (var res in resources) {
-      res = resources[res];
-      var resultTemp = [];
-      resultTemp.push({
-        url: res.secure_url,
-        height: res.height,
-        width: res.width,
-      });
-      result.push(resultTemp);
-    }
+      for (var res in resources) {
+        res = resources[res];
+        const resultTemp = [];
+        resultTemp.push({
+          src: config.height
+            ? res.secure_url.replace("/upload/", `/upload/h_${config.height}/`)
+            : res.secure_url,
+          height: config.height || res.height,
+          width: config.height
+            ? +(res.width * (config.height / res.height)).toFixed(0)
+            : res.width,
+        });
+        result.push(resultTemp);
+      }
 
-    if (more) {
-      await listResources(options, save_path, result, more);
-    } else {
-      console.info("done");
-      fs.writeFile(
-        save_path,
-        `export const images = ${JSON.stringify(result.map((url) => url[0]))}`,
-        function (err) {
-          if (err) {
-            return console.log(err);
+      if (more) {
+        await listResources(config, result, more);
+      } else {
+        console.info("Done 🎉");
+        fs.writeFile(
+          config.save_path,
+          `export const images = ${JSON.stringify(
+            result.map((url) => url[0])
+          )}`,
+          function (err) {
+            if (err) {
+              return console.log(err);
+            }
+            console.log("The file was saved!");
           }
-          console.log("The file was saved!");
-        }
-      );
+        );
+      }
     }
-  });
+  );
 }
 
-fetchCloudinaryAssets(
-  "Community Solidarity/Whole Foods Commack",
-  "./src/constants/wfc-album.ts"
-);
+const options = {
+  type: "upload",
+  max_results: 200,
+};
 
-fetchCloudinaryAssets(
-  "Community Solidarity/CS Lite Album",
-  "./src/constants/photo-album.ts"
-);
+fetchCloudinaryAssets({
+  save_path: "./src/constants/wfc-album.ts",
+  height: 150,
+  options: {
+    ...options,
+    prefix: "Community Solidarity/Whole Foods Commack",
+  },
+});
+
+fetchCloudinaryAssets({
+  save_path: "./src/constants/photo-album.ts",
+  options: {
+    ...options,
+    prefix: "Community Solidarity/CS Lite Album",
+  },
+});
