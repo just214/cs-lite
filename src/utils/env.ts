88fac8fd1c712ea626804e46ce6b96abc0ede9d4
just214 <21136155+just214@users.noreply.github.com>
2023-06-23 14:45:14 -0500

    build: added supabase env variables

---
+++ src/utils/env.ts
@@ -0,0 +1,11 @@
+export const SUPABASE_URL = process.env.SUPABASE_URL as string;
+export const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY as string;
+export const AIRTABLE_TOKEN = process.env.AIRTABLE_TOKEN as string;
+export const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID as string;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY as string;
+export const CLOUDINARY_API_SECRET = process.env
+  .CLOUDINARY_API_SECRET as string;
+export const CLOUDINARY_CLOUD_NAME = process.env
+  .CLOUDINARY_CLOUD_NAME as string;
+export const CLOUDINARY_URL = process.env.CLOUDINARY_URL as string;
+export const NODE_ENV = process.env.NODE_ENV as string;
